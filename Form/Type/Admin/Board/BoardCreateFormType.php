62ea41868ec8123ebba596210831c74524bd3719
joschi127 <127.joschi@gmail.com>
2015-01-07 20:39:09 +0100

    Allow children boards, also allow setting parent when creating new board

--- Form/Type/Admin/Board/BoardCreateFormType.php
+++ Form/Type/Admin/Board/BoardCreateFormType.php
@@ -94,6 +94,14 @@ class BoardCreateFormType extends AbstractType
                     'translation_domain' => 'CCDNForumForumBundle',
                 )
             )
+            ->add('parent', null,
+                array(
+                    'required' => false,
+                    'property' => 'name',
+                    'label' => 'board.parent-label',
+                    'translation_domain' => 'CCDNForumForumBundle',
+                )
+            )
             ->add('name', 'text',
                 array(
                     'label'              => 'board.name-label',
